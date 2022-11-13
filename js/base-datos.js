cde937771010ac10edf9c146cda986a55330924d
GabyDs <gabydsch2001@gmail.com>
2022-11-13 16:27:04 -0300

    Tabla dinamica y base de datos

---
+++ js/base-datos.js
@@ -0,0 +1,27 @@
+var Datastore = require('nedb')
+
+// Persistent datastore with automatic loading
+let db = new Datastore({filename: 'db/registro.db', autoload: true})
+
+exports.agregarBarra = function(longitud, elasticidad, base, altura,condicionBorde, cargaDist) {
+    var barra = {
+        longitud: longitud,
+        elasticidad: elasticidad,
+        base: base,
+        altura: altura,
+        condicionBorde: condicionBorde,
+        cargaDist: cargaDist,
+    }
+
+    db.insert(barra, function(err, nuevoObjeto) {
+ 
+    })
+}
+
+exports.obtenerBarras = function (operacion) {
+    db.find({}, function(err, barras) {
+        if (barras) {
+            operacion(barras)
+        }
+    })
+}
--- js/logica.js
+++ js/logica.js
@@ -1,12 +1,19 @@
+const baseDatos = require('./js/base-datos.js')
+
 class GestorApp{
     constructor(){
+        this.frmNuevaBarra = document.getElementById('datos-barras')
+
         this.longBarra = document.getElementById('long-barra')
         this.moduloElast = document.getElementById('elasticidad-barra')
         this.baseBarra = document.getElementById('base-barra')
         this.alturaBarra = document.getElementById('altura-barra')
         this.cargaDistBarra = document.getElementById('carga-dist-barra')
-        this.condicionBorde = document.getElementById('condicion-borde').value
+        this.condicionBorde = document.getElementById('condicion-borde')
+
         this.btnCargarBarra = document.getElementById('btn-cargar-barra')
+
+        this.tablaBarras = document.getElementById('tabla-barras')
 
         this.cargarRegistroBarras()
 
@@ -14,12 +21,50 @@ class GestorApp{
     }
 
     agregarEventListeners(){
-
+        this.frmNuevaBarra.addEventListener('submit', this.crearRegistroBarras.bind(this))
     }
 
-    cargarRegistroBarras(evento){
+    crearRegistroBarras(evento){
         evento.preventDefault()
+
+        baseDatos.agregarBarra(
+            this.longBarra.value,
+            this.moduloElast.value,
+            this.baseBarra.value,
+            this.alturaBarra.value,
+            this.condicionBorde.options[this.condicionBorde.selectedIndex].text,
+            this.cargaDistBarra.value
+        )
+        
+        this.longBarra.value = ''
+        this.moduloElast.value = ''
+        this.baseBarra.value = ''
+        this.alturaBarra.value = ''
+        this.cargaDistBarra.value = ''
+        //this.condicionBorde.value = ''
+        
+        this.cargarRegistroBarras()
+    }
+
+    generarHtmlRegistroBarra(barra){
+        console.log(barra)
+        return `<tr>
+            <td>${barra.longitud}</td>
+            <td>${barra.elasticidad}</td>
+            <td>${barra.base}</td>
+            <td>${barra.altura}</td>
+            <td>${barra.condicionBorde}</td>
+            <td>${barra.cargaDist}</td>
+        </tr>`
+    }
+
+    cargarRegistroBarras(){
+        baseDatos.obtenerBarras( (barras) => {
+            let html = barras.map(this.generarHtmlRegistroBarra).join('')
+
+            this.tablaBarras.innerHTML = html
+        })
     }
 }
 
-let gestor = new GestorApp()
+let gestorBarras = new GestorApp()
